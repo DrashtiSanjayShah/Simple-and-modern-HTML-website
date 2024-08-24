d8096b540f5c3ba584ca804240b8b922023d8c38
DrashtiS <94853646+DrashtiSanjayShah@users.noreply.github.com>
2024-08-24 20:46:50 +0000

    Make responsive

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburgerMenu.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+    });
+});
